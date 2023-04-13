ec81c083cbea43329c000838881ba46e93fb74fa
Chandankl5 <chandankl0310@gmail.com>
2023-04-14 02:34:43 +0530

    feat: Integrate Components into App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { SearchInput } from './modules/Search/components/SearchInput';
+import { SearchResults } from './modules/Search/components/SearchResults';
+import { fetchUsersList } from './modules/Search/ducks/UsersSlice';
+
 import './App.css';
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Fetch users List on Mount
+    dispatch(fetchUsersList());
+  }, [])
+
   return (
     <div className="App">
+      <SearchInput />
+      <SearchResults />
     </div>
   );
 }
